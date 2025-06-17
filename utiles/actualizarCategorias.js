05339047f04d4796871b4a937f95403ca68285b0
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-06-17 17:35:54 -0300

    Update actualizarCategorias.js

--- utiles/actualizarCategorias.js
+++ utiles/actualizarCategorias.js
@@ -1,7 +1,11 @@
 // utils/actualizarCategorias.js
 const fs = require('fs');
 const path = require('path');
-const { guardarYSubirCategorias } = require('../guardarCategoriasGit');
+const { guardarYSubirCatA } = require('../git/guardarGit_Cat_A.js');
+const { guardarYSubirCatB } = require('../git/guardarGit_Cat_B.js');
+const { guardarYSubirCatC } = require('../git/guardarGit_Cat_C.js');
+const { guardarYSubirCatD } = require('../git/guardarGit_Cat_D.js');
+const { guardarYSubirCatE } = require('../git/guardarGit_Cat_E.js');
 const config = require('../botConfig.json');
 
 async function actualizarCategoriasDesdeRoles(guild) {
@@ -33,9 +37,21 @@ async function actualizarCategoriasDesdeRoles(guild) {
     const archivo = path.join(carpeta, `categoria_${letra}.json`);
     fs.writeFileSync(archivo, JSON.stringify(jugadores, null, 2), 'utf8');
     console.log(`📂 Actualizado categoria_${letra}.json con ${jugadores.length} jugadores.`);
-  }
 
-  // Subida automática a GitHub
-  await guardarYSubirCategorias();
+    // Subida automática a GitHub por cada categoría
+    if (letra === 'a') {
+      await guardarYSubirCatA();
+    } else if (letra === 'b') {
+      await guardarYSubirCatB();
+    } else if (letra === 'c') {
+      await guardarYSubirCatC();
+    } else if (letra === 'd') {
+      await guardarYSubirCatD();
+    } else if (letra === 'e'){
+      await guardarYSubirCatE();
+    } 
+  }
 }
+
 module.exports = { actualizarCategoriasDesdeRoles };
+
