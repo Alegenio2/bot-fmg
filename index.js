9e7f90d65fcd3d4bbe0a4feec9775d56bd603483
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-06-18 13:59:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -575,7 +575,7 @@ async function guardarLiga(liga, filePath, letraDivision, interaction) {
     fs.writeFileSync(filePath, JSON.stringify(liga, null, 2), 'utf8');
     console.log(`✅ Resultado guardado en liga_${letraDivision}.json`);
 
-    const { subirTodasLasLigas } = require('../git/guardarLigasGit');
+    const { subirTodasLasLigas } = require('./git/guardarLigasGit');
     await subirTodasLasLigas();
   } catch (error) {
     console.warn('⚠️ No se pudo subir a GitHub:', error.message);
