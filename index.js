703e24ed8d033ac499c0f4b4310716f22179251f
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-07-03 20:58:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const keep_alive = require("./keep_alive.js")
+const keep_alive = require("./keep_alive.js");
+require('./registro-comandos.js');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
