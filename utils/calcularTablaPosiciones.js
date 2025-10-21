bbd856d316630bfd6ae43a2125567349634260cb
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-10-21 12:38:53 -0300

    Update calcularTablaPosiciones.js

--- utils/calcularTablaPosiciones.js
+++ utils/calcularTablaPosiciones.js
@@ -9,41 +9,41 @@ function calcularTablaPosiciones(torneo) {
       if (!tablas[nombreGrupo]) tablas[nombreGrupo] = {};
 
       for (const partido of grupo.partidos || []) {
-        const { equipo1Nombre: eq1, equipo2Nombre: eq2, resultado } = partido;
-        if (!eq1 || !eq2) continue;
+        const { equipo1Id: eq1Id, equipo1Nombre: eq1Nombre, equipo2Id: eq2Id, equipo2Nombre: eq2Nombre, resultado } = partido;
+        if (!eq1Id || !eq2Id) continue;
 
         // Inicializar equipos si no existen
-        if (!tablas[nombreGrupo][eq1]) {
-          tablas[nombreGrupo][eq1] = { jugados: 0, ganados: 0, perdidos: 0, setsGanados: 0, setsPerdidos: 0, puntos: 0 };
+        if (!tablas[nombreGrupo][eq1Id]) {
+          tablas[nombreGrupo][eq1Id] = { nombre: eq1Nombre, jugados: 0, ganados: 0, perdidos: 0, setsGanados: 0, setsPerdidos: 0, puntos: 0 };
         }
-        if (!tablas[nombreGrupo][eq2]) {
-          tablas[nombreGrupo][eq2] = { jugados: 0, ganados: 0, perdidos: 0, setsGanados: 0, setsPerdidos: 0, puntos: 0 };
+        if (!tablas[nombreGrupo][eq2Id]) {
+          tablas[nombreGrupo][eq2Id] = { nombre: eq2Nombre, jugados: 0, ganados: 0, perdidos: 0, setsGanados: 0, setsPerdidos: 0, puntos: 0 };
         }
 
         if (resultado) {
-          const puntosEq1 = resultado[eq1];
-          const puntosEq2 = resultado[eq2];
+          const puntosEq1 = resultado[eq1Id];
+          const puntosEq2 = resultado[eq2Id];
           if (typeof puntosEq1 !== 'number' || typeof puntosEq2 !== 'number') continue;
 
-          tablas[nombreGrupo][eq1].jugados++;
-          tablas[nombreGrupo][eq2].jugados++;
-          tablas[nombreGrupo][eq1].setsGanados += puntosEq1;
-          tablas[nombreGrupo][eq1].setsPerdidos += puntosEq2;
-          tablas[nombreGrupo][eq2].setsGanados += puntosEq2;
-          tablas[nombreGrupo][eq2].setsPerdidos += puntosEq1;
+          tablas[nombreGrupo][eq1Id].jugados++;
+          tablas[nombreGrupo][eq2Id].jugados++;
+          tablas[nombreGrupo][eq1Id].setsGanados += puntosEq1;
+          tablas[nombreGrupo][eq1Id].setsPerdidos += puntosEq2;
+          tablas[nombreGrupo][eq2Id].setsGanados += puntosEq2;
+          tablas[nombreGrupo][eq2Id].setsPerdidos += puntosEq1;
 
           if (puntosEq1 > puntosEq2) {
-            tablas[nombreGrupo][eq1].ganados++;
-            tablas[nombreGrupo][eq2].perdidos++;
-            tablas[nombreGrupo][eq1].puntos += 3;
+            tablas[nombreGrupo][eq1Id].ganados++;
+            tablas[nombreGrupo][eq2Id].perdidos++;
+            tablas[nombreGrupo][eq1Id].puntos += 3;
           } else if (puntosEq2 > puntosEq1) {
-            tablas[nombreGrupo][eq2].ganados++;
-            tablas[nombreGrupo][eq1].perdidos++;
-            tablas[nombreGrupo][eq2].puntos += 3;
+            tablas[nombreGrupo][eq2Id].ganados++;
+            tablas[nombreGrupo][eq1Id].perdidos++;
+            tablas[nombreGrupo][eq2Id].puntos += 3;
           } else {
             // Empate (si aplica)
-            tablas[nombreGrupo][eq1].puntos += 1;
-            tablas[nombreGrupo][eq2].puntos += 1;
+            tablas[nombreGrupo][eq1Id].puntos += 1;
+            tablas[nombreGrupo][eq2Id].puntos += 1;
           }
         }
       }
@@ -53,8 +53,9 @@ function calcularTablaPosiciones(torneo) {
   // Ordenar los grupos
   for (const grupo in tablas) {
     tablas[grupo] = Object.entries(tablas[grupo])
-      .map(([nombre, datos]) => ({
-        equipo: nombre,
+      .map(([id, datos]) => ({
+        id,           // Guardamos ID para referencias futuras
+        nombre: datos.nombre,
         ...datos,
         diferencia: datos.setsGanados - datos.setsPerdidos
       }))
@@ -69,3 +70,4 @@ function calcularTablaPosiciones(torneo) {
 }
 
 module.exports = { calcularTablaPosiciones };
+
