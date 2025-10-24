468cc38f9c6a6f44d4212fc2956b543141b4ebf7
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-10-24 12:23:30 -0300

    Update coordinado_equipos.js

--- comandos/coordinado_equipos.js
+++ comandos/coordinado_equipos.js
@@ -86,7 +86,7 @@ module.exports = {
     const fecha = interaction.options.getString('fecha');
     const horario = interaction.options.getString('horario');
 
-    const filePath = path.join(__dirname, '..', 'torneos', `${torneoId}.json`);
+    const filePath = path.join(__dirname, '..', 'torneos', `toreno_${torneoId}.json`);
 
     let torneo;
     try {
@@ -154,6 +154,7 @@ module.exports = {
     }
   }
 };
+
 
 
 
